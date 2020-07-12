6e034a2d39c7bb18e1a036c97800dce68f49d9bc
Koala <bozonedouard@gmail.com>
2020-07-12 15:43:17 +0200

    chore(build): resolve json files in Jest

--- packages/nuxt/jest.config.js
+++ packages/nuxt/jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html', 'json'],
   coverageDirectory: '../../coverage/packages/nuxt',
 };
