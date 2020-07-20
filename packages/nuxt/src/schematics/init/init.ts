08d0bcfff78f4e2e49e47a149d0cc2aa8f38a710
Koala <bozonedouard@gmail.com>
2020-07-20 12:21:00 +0200

    chore(schematics): improve update dependencies

--- packages/nuxt/src/schematics/init/init.ts
+++ packages/nuxt/src/schematics/init/init.ts
@@ -6,6 +6,23 @@ import {
 } from '@nrwl/workspace';
 import { Schema } from './schema';
 
+const updateDependencies = addDepsToPackageJson(
+  {
+    nuxt: '^2.13.0',
+    '@nuxt/typescript-runtime': '^1.0.0',
+  },
+  {
+    '@nuxt/babel-preset-app': '^2.13.3',
+    '@nuxt/typescript-build': '^1.0.3',
+    '@vue/test-utils': '^1.0.3',
+    'babel-core': '7.0.0-bridge.0',
+    jest: '^26.0.1',
+    'ts-jest': '^26.1.0',
+    'vue-jest': '^3.0.4',
+    'vue-loader': '^15.9.3',
+  }
+);
+
 export default function (schema: Schema): Rule {
   return chain([
     setDefaultCollection('@vue/nuxt'),
@@ -15,23 +32,6 @@ export default function (schema: Schema): Rule {
     schema.e2eTestRunner === 'cypress'
       ? addPackageWithInit('@nrwl/cypress')
       : noop(),
-    addPackageWithInit('@nrwl/web', schema),
-    addDepsToPackageJson(
-      {
-        nuxt: '^2.13.0',
-        '@nuxt/typescript-runtime': '^1.0.0',
-        'core-js': '^3.6.5',
-      },
-      {
-        '@nuxt/babel-preset-app': '^2.13.3',
-        '@nuxt/typescript-build': '^1.0.3',
-        '@vue/test-utils': '^1.0.3',
-        'babel-core': '7.0.0-bridge.0',
-        jest: '^26.0.1',
-        'ts-jest': '^26.1.0',
-        'vue-jest': '^3.0.4',
-        'vue-loader': '^15.9.3',
-      }
-    ),
+    updateDependencies,
   ]);
 }
