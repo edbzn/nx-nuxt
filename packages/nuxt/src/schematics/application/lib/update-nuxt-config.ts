8f9058728bf9b9d7f9ffbd2d1c4c077fefb2420c
Edouard Bozon <bozonedouard@gmail.com>
2020-11-30 18:09:15 +0100

    fix(schematics): use project name

--- packages/nuxt/src/schematics/application/lib/update-nuxt-config.ts
+++ packages/nuxt/src/schematics/application/lib/update-nuxt-config.ts
@@ -9,7 +9,7 @@ export function updateNuxtConfig(options: NormalizedSchema): Rule {
         target: '${options.target}',
         ssr: ${options.mode === 'universal'},
         head: {
-          title: process.env.npm_package_name || '',
+          title: ${options.name} || '',
           meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
